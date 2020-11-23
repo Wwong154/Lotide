4b7ba671090da3815c41d0d0a71c2a7b33d54697
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-23 19:44:04 +0000

    add function assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+  return console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+};
+
+/*
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+*/

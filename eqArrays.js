cbed679aca48466a57f7f96d64204e0601e5bc4c
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-24 17:42:37 +0000

    Added the js eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let x in actual) {
+    if (actual[x] !== expected[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//test con
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+//
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

6ff6906ce954893f26361eeba894521248c38c0e
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-25 19:28:43 +0000

    letter position added

--- letterPositions.js
+++ letterPositions.js
@@ -15,12 +15,20 @@ const eqArrays = function(actual, expected) {
   }
   return true;
 };
-const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  return results;
+const letterPositions = function(input) {
+  let result = {};
+  for (let index in input) {  // loop with index
+    if (result[`${input[index]}`]) { // if key is avaliable , log index
+      result[`${input[index]}`].push(Number(index));
+    } else if (input[index] === ' '){ // if character is space, ingore
+    } else { // log key that is not in result yet
+      result[`${input[index]}`] = [Number(index)];
+    }
+  }
+  return result;
 };
 
 
 //test
-letterPositions("hello")
+assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]);
+assertArraysEqual(letterPositions("hello").e, [1]);

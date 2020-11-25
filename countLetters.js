c5084dcae78606d5d947642535238369a888b06a
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-25 18:55:27 +0000

    added countLetter.js and modified assertEqual to better serve the purpose

--- countLetters.js
+++ countLetters.js
@@ -9,10 +9,17 @@ const assertEqual = function(actual, expected) {
   }
   return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
-
-/*
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-*/
+const countLetters = function(input) {
+  let holder = input.split(" ").join("");
+  let result = {};
+  for (let letters of holder) {
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
+//test
+console.log(assertEqual(countLetters("lighthouse in the house"),{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }));
---
+++ letterPositions.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === false) {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let x in actual) {
+    if (actual[x] !== expected[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
+
+
+//test
+letterPositions("hello")

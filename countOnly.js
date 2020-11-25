af629108d82e2ca62473305db1e6ffeffe7bf9b9
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-25 18:22:05 +0000

    add countOnly to the repo

---
+++ countOnly.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+const countOnly = function(allItem, itemsToCount) {
+  let result = {};
+  for (let items of allItem) {
+    if (itemsToCount[items]) {
+      if (result[items]) {
+        result[items] += 1;
+      } else {
+        result[items] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+//test
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
